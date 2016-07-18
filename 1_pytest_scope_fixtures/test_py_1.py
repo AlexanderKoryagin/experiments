fae562b8121b6e2d69915b46f95e1ee34ab14d9c
Alexander Koryagin <akoryag@mera.ru>
2016-07-18 15:54:14 +0300

    first commit

---
+++ 1_pytest_scope_fixtures/test_py_1.py
@@ -0,0 +1,28 @@
+# sudo -H pip install pytest
+# sudo -H pip install pytest-pep8   # py.test -s --pep8
+# sudo -H pip install pytest-flakes # py.test -s --flakes
+# py.test -s
+# py.test -v -ra -s
+
+import pytest
+
+pytestmark = pytest.mark.usefixtures(
+    'function_scope', 'class_scope', 'module_scope', 'session_scope')
+
+
+class TestClass1(object):
+
+    def test_1(self):
+        print 'TEST 1 (test_py_1.py: TestClass1:test_1)'
+
+    def test_2(self):
+        print 'TEST 2 (test_py_1.py: TestClass1:test_2)'
+
+
+class TestClass2(object):
+
+    def test_3(self):
+        print 'TEST 3 (test_py_1.py: TestClass2:test_1)'
+
+    def test_4(self):
+        print 'TEST 4 (test_py_1.py: TestClass2:test_1)'
---
+++ 2_pytest_parametrize/test_param_test.py
@@ -0,0 +1,18 @@
+# http://docs.pytest.org/en/latest/parametrize.html
+# py.test -v -ra -s -k test_param_fixture.py
+
+import pytest
+
+
+@pytest.mark.parametrize('param', ['one', 'two', 'three', 'four'])
+def test_something1(param):
+    """Tests usage of PyTest for case parametrize"""
+    print '\nTest with value of "param" = "{0}"'.format(param)
+
+
+@pytest.mark.parametrize('param1', ['one', 'two', 'three', 'four'])
+@pytest.mark.parametrize('param2', [1, 2, 3, 4])
+def test_something2(param1, param2):
+    """Tests usage of PyTest for case parametrize"""
+    print '\nTest with value of "param1" = "{0}"'.format(param1)
+    print '\nTest with value of "param2" = "{0}"'.format(param2)
