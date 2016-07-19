e5bf00f25438b7f21271a7f8b9db343368d196b8
Alexander Koryagin <akoryag@mera.ru>
2016-07-19 10:04:36 +0300

    Add fixtures parametrize

---
+++ 2_pytest_parametrize/test_param_fixtures2.py
@@ -0,0 +1,33 @@
+# http://stackoverflow.com/questions/22171681/pytest-parameterized-test-with-cartesian-product-of-arguments
+
+
+import itertools
+import pytest
+
+numbers = [1, 2, 3, 4, 5]
+vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ['x', 'y', 'z']
+
+
+@pytest.mark.parametrize(
+    'number,vowel,consonant', itertools.product(numbers, vowels, consonants))
+def test1(number, vowel, consonant):
+    print 'First test: {0}, {1}, {2}'.format(number, vowel, consonant)
+
+# -------------------------------------------
+
+@pytest.fixture(params=numbers)
+def number(request):
+    return request.param
+
+@pytest.fixture(params=vowels)
+def vowel(request):
+    return request.param
+
+@pytest.fixture(params=consonants)
+def consonant(request):
+    return request.param
+
+
+def test2(number, vowel, consonant):
+    print 'Second test: {0}, {1}, {2}'.format(number, vowel, consonant)
