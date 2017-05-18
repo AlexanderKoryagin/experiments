6741b72ac9acce9711861268392b87209a78859a
Alexander Koryagin <alexander.koryagin@tecswepoint.se>
2017-05-18 16:41:09 +0300

    More monkeypatch

--- experiments/5_monkeypatch/test_monkeypatch.py
+++ experiments/5_monkeypatch/test_monkeypatch.py
@@ -15,3 +15,24 @@ def test_mytest1(monkeypatch):
     print get_dir_content('/home/')  # ['dir1', 'dir2', 'dir3']
     monkeypatch.undo()
     print get_dir_content('/home/')  # ['alex']
+
+
+# -------------------------------------
+import datetime
+import pytest
+
+FAKE_TIME = datetime.datetime(2020, 12, 25, 17, 05, 55)
+
+@pytest.fixture
+def patch_datetime_now(monkeypatch):
+
+    class mydatetime:
+        @classmethod
+        def now(cls):
+            return FAKE_TIME
+
+    monkeypatch.setattr(datetime, 'datetime', mydatetime)
+
+
+def test_patch_datetime(patch_datetime_now):
+    assert datetime.datetime.now() == FAKE_TIME
